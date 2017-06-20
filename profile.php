d5a2680b2578ad18629bd2443ec791914b1ca9b2
Yarj42 <yaroslav.j42@gmail.com>
2017-06-20 16:25:55 +0300

    Login profile.php

---
+++ profile.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html lang="en">
+ <head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<title>Title Goes Here</title> 
+
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/layout.css">
+ </head> 
+<body> 	
+	<div class="back"></div>
+	<div class="pan back"></div>
+	<div class="pan center">
+		<div class="login">
+			
+			<h1>VSOC</h1>
+
+			<p>Progile</p>
+			
+				
+
+			</form>
+		</div>
+	</div>
+</body>
+</html> 
