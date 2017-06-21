b5c61b9800eb4f03df59aca06f9b30c6b862297a
Yarj42 <yaroslav.j42@gmail.com>
2017-06-21 14:53:34 +0300

    work

--- profile.php
+++ profile.php
@@ -10,30 +10,55 @@
 	<link rel="stylesheet" type="text/css" href="css/layout.css">
 	<link rel="stylesheet" href="css/font-awesome.min.css">
 
+	<link rel="stylesheet" type="text/css" href="css/scroll.css">
  </head> 
 <body> 	
 	<div class="back"></div>
-	<div class="pan back"></div>
-	<div class="pan center">
+	
+	<div class="pan center 
+	col-xs-12 
+	col-sm-10 
+	col-md-8
+	">
 		<nav class="col-xs-12 nav m_bottom25">
-			<a href="index.php" class="link link-main">VSOC</a>	
-			<div class="sep"></div>				
+			<a href="index.php" class="link link-main">VSOC</a>
+
+			<div class="sep"></div>		
+			<a href="profile.php"><i class="fa fa-user" aria-hidden="true"></i></a>		
+			<div class="sep"></div>		
 			
-			<a href="progile.php"><i class="fa fa-user" aria-hidden="true"></i></a>
-			<a href="#"><i class="fa fa-comment" aria-hidden="true"></i></a>
-			<a href="#"><i class="fa fa-bell" aria-hidden="true"></i></a>			
+			
+			<a href="#" class="link">
+				<i class="fa fa-comment" aria-hidden="true"></i>
+				<sub>3</sub>
+			</a>
+			<a href="#" class="link">
+				<i class="fa fa-bell" aria-hidden="true"></i>
+				<sub>52</sub>
+			</a>			
 		</nav>
 		<div class="col-xs-12 page">	
-		<div class="col-xs-5 main-photo">	
-			<img src="https://s-media-cache-ak0.pinimg.com/236x/37/aa/5f/37aa5fad95cc8d2a8aa136843e173ee9.jpg" alt="">
-		</div>
-		<div class="col-xs-7">	
-			<h3 class="p0 m0"><b>Camillo Carter</b></h3><br>	
-			<p> <b>Age:</b> 61 y.o.</p>
-			<p> <b>Profession: </b>photographer</p>
-			<p>  <b>Location:</b> Odessa</p>
-			<p> <b>About:</b> I had started to work as a weekend sports photographer in 1983. In 1984, he moved on to work for the Johannesburg Star, went on exposing the brutality of apartheid.
-Carter was the first to photograph a public execution "necklacing" by black Africans in South Africa in the mid-1980s. Carter later spoke of the images: "I was appalled at what they were doing. But then people started talking about those pictures... then I felt that maybe my actions hadn't been at all bad. Being a witness to something this horrible wasn't necessarily such a bad thing to do</p>
+		<div class="row">
+			<div class="col-xs-5 main-photo">	
+				<img src="https://s-media-cache-ak0.pinimg.com/236x/37/aa/5f/37aa5fad95cc8d2a8aa136843e173ee9.jpg" alt="">
+			</div>
+			<div class="col-xs-7 ">	
+				<div class="profile-btns">
+					<a href="#"><i class="fa fa-comment" aria-hidden="true"></i></a>
+				</div>
+				<h3 class="p0 m0" id="profile-head"><b id="profile-name">Camillo Carter</b> <small>online</small></h3><br>	
+
+					<div class="profile-info">
+				<p> <b>Age:</b> 61 y.o.</p>
+				<p> <b>Profession: </b>photographer</p>
+				<p>  <b>Location:</b> Odessa</p>
+				<p> <b>About:</b> I had started to work as a weekend sports photographer in 1983. In 1984, he moved on to work for the Johannesburg Star, went on exposing the brutality of apartheid.
+	Carter was the first to photograph a public execution "necklacing" by black Africans in South Africa in the mid-1980s. Carter later spoke of the images: "I was appalled at what they were doing. But then people started talking about those pictures... then I felt that maybe my actions hadn't been at all bad. Being a witness to something this horrible wasn't necessarily such a bad thing to do.
+I had started to work as a weekend sports photographer in 1983. In 1984, he moved on to work for the Johannesburg Star, went on exposing the brutality of apartheid.
+	Carter was the first to photograph a public execution "necklacing" by black Africans in South Africa in the mid-1980s. Carter later spoke of the images: "I was appalled at what they were doing. But then people started talking about those pictures... then I felt that maybe my actions hadn't been at all bad. Being a witness to something this horrible wasn't necessarily such a bad thing to do
+	</p>
+				</div>
+			</div>		
 		</div>
 
 		</div>
